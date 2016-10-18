5b273afb18b7d520bde44f6f594038c79787e457
Eric Zuckerman <ericzuckerman@Erics-MacBook-Pro.local>
2016-10-17 19:03:21 -0700

    game function works

--- gamePlay.js
+++ gamePlay.js
@@ -6,54 +6,59 @@
 
 // runners on base will always move up the same number of bases as the hit
 // no double plays, sacrifices or stolen bases in this version
+var Player = require("./player.js");
 
-var homeba = .320;
-var homebaa = .250;
-var awayba = .300;
-var awaybaa = .225;
 var inn = 1;
 var outs = 0;
 var bases = [false, false, false];
+var batter;
+var pitcher;
 var runs = 0;
 var aruns = 0;
 var hruns = 0;
-var hrpct = 300;
-var tplpct = 100;
-var dblpct = 250;
-var sglpct = 350;
-var kpct = 144;
-//kpct = SO divided by IPout times 1000
+var ord = 0;
 
-function pitch(){
-	var hitprob = (homeba + awaybaa) / 2;
+
+function startGame(teamA, teamH){
+	awayOffense(teamA, teamH);
+};
+function order(){
+	ord++;
+	if (ord > 8){
+		ord = 0;
+	};
+};
+function pitch(batter, pitcher){
+	var hitprob = (batter.ba + pitcher.baOpp) / 2;
 	hitprob = hitprob.toFixed(3);
-	hitprob = hitprob * 1000;
 	var ranNum = Math.floor(Math.random() * 1000) + 1;
 	if (ranNum < hitprob){
-		hit();
+		hit(batter, pitcher);
+		order();
 		console.log("Outs: " + outs);
 		console.log("Runs: " + runs);
 		console.log("Inning: " + inn);
 	}
 	else {
-		out();
+		out(batter, pitcher);
+		order();
 		console.log("Outs: " + outs);
 		console.log("Runs: " + runs);
 		console.log("Inning: " + inn);
 	};
 };
 
-function hit(){
+function hit(batter, pitcher){
 	var hitNum = Math.floor(Math.random() * 1000) + 1;
-	if (hitNum < sglpct) {
+	if (hitNum < batter.singlepct) {
 		console.log("SINGLE");
 		single();
 	}
-	else if (hitNum < (sglpct + dblpct)) {
+	else if (hitNum < (batter.singlepct + batter.doublepct)) {
 		console.log("DOUBLE");
 		double();
 	}
-	else if (hitNum < (sglpct + dblpct + tplpct)) {
+	else if (hitNum < (batter.singlepct + batter.doublepct + batter.triplepct)) {
 		console.log("TRIPLE");
 		triple();
 	}
@@ -63,9 +68,10 @@ function hit(){
 	};
 };
 
-function out(){
-	var SOnum = Math.floor(Math.random() * 1000) + 1;
-	if (SOnum < kpct){
+function out(batter, pitcher){
+	var soNum = Math.floor(Math.random() * 1000) + 1;
+	var soProb = (batter.soPctBatter + pitcher.soPctPitcher) / 2;
+	if (soNum < soProb){
 		outs++
 		console.log("STRIKE OUT");
 	}
@@ -149,30 +155,34 @@ function homerun(){
 	runs++;
 	return
 }; 
- function awayOffense(){
+ function awayOffense(teamA, teamH){
+	batter = teamA[ord];
+	pitcher = teamH[8];
 	if (inn > 9){
 		aruns = runs;
 		console.log("The away team scored: " + aruns + " runs.");
 		inn = 1;
 		outs = 0;
 		runs = 0;
-		homeOffense();
+		ord = 0;
+		homeOffense(teamA, teamH);
 		return;
 	}
 	if (outs < 3){
-		pitch();
-		awayOffense();
+		pitch(batter, pitcher);
+		awayOffense(teamA, teamH);
 	}
 	else {
 		bases = [false, false, false];
 		outs = 0;
 		inn++
-		awayOffense();
+		awayOffense(teamA, teamH);
 	};
 };
 
-function homeOffense(){
-
+function homeOffense(teamA, teamH){
+	batter = teamH[ord];
+	pitcher = teamA[8];
 	if (inn > 9){
 		hruns = runs;
 		gameOver();
@@ -186,8 +196,8 @@ function homeOffense(){
 		}
 		else {
 			if (outs < 3){
-				pitch();
-				homeOffense();
+				pitch(batter, pitcher);
+				homeOffense(teamA, teamH);
 			}
 			else {
 				hruns = runs;
@@ -197,20 +207,20 @@ function homeOffense(){
 		};
 	}
 	else {
-		homeBatting();
+		homeBatting(teamA, teamH);
 	};
 };
 
 function homeBatting() {
 	if (outs < 3){
-		pitch();
-		homeOffense();
+		pitch(batter, pitcher);
+		homeOffense(teamA, teamH);
 	}
 	else {
 		bases = [false, false, false];
 		outs = 0;
 		inn++;
-		homeOffense();
+		homeOffense(teamA, teamH);
 	};
 };
 
@@ -219,5 +229,32 @@ function gameOver(){
 	console.log("Inning at end: " + inn);
 	return;
 };
-awayOffense();
+
+
+ var playerA1 = new Player("Pete", "Rose", "1B", 685, 185, 33, 10, 9, 58);
+ var playerA2 = new Player("Bake", "McBride", "RF", 554, 171, 42, 1, 1, 33);
+ var playerA3 = new Player("Garry", "Maddox", "CF", 549, 142, 31, 3, 11, 52);
+ var playerA4 = new Player("Mike", "Schmidt", "3B", 548, 157, 25, 8, 48, 119);
+ var playerA5 = new Player("Greg", "Luzinski", "LF", 386, 84, 19, 1, 19, 100);
+ var playerA6 = new Player("Bob", "Boone", "C", 480, 110, 23, 1, 9, 41);
+ var playerA7 = new Player("Larry", "Bowa", "SS", 540, 144, 16, 4, 2, 28);
+ var playerA8 = new Player("Manny", "Trillo", "2B", 531, 155, 25, 9, 7, 46);
+ var playerA9 = new Player("Steve", "Carlton", "P", 101, 19, 1, 0, 0, 21, .21, 912, 286);
+
+ var playerH1 = new Player("Jimmy", "Rollins", "SS", 556, 154, 38, 9, 11, 55);
+ var playerH2 = new Player("Shane", "Victorino", "CF", 570, 167, 30, 8, 14, 69);
+ var playerH3 = new Player("Chase", "Utley", "2B", 607, 177, 41, 4, 33, 104);
+ var playerH4 = new Player("Ryan", "Howard", "1B", 610, 153, 26, 4, 48, 199);
+ var playerH5 = new Player("Pat", "Burrell", "LF", 536, 134, 33, 3, 33, 136);
+ var playerH6 = new Player("Jayson", "Werth", "RF", 418, 114, 16, 3, 24, 119);
+ var playerH7 = new Player("Pedro", "Feliz", "3B", 425, 106, 19, 2, 14, 54);
+ var playerH8 = new Player("Carlos", "Ruiz", "C", 320, 70, 14, 0, 4, 38);
+ var playerH9 = new Player("Cole", "Hamels", "P", 76, 17, 2, 0, 0, 29, .227, 682, 196);
+
+
+
+ var teamA = [playerA1, playerA2, playerA3, playerA4, playerA5, playerA6, playerA7, playerA8, playerA9];
+ var teamH = [playerH1, playerH2, playerH3, playerH4, playerH5, playerH6, playerH7, playerH8, playerH9];
+
+startGame(teamA, teamH);
 
