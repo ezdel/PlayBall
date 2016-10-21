673da482ead3fc324d59d497e13ed153a14c868f
Jose Alvaro Garcia <jag040393@gmail.com>
2016-10-20 17:18:13 -0700

    Fixed issues with select

--- public/js/app.js
+++ public/js/app.js
@@ -22,20 +22,53 @@ $('#submitTeam').on('click', function(e){
 
 // ---LINEUP---
 $('#playerName').keypress(function () {
+	var positionType = $("#positionType").val();
   $("#results").empty();
+if(positionType === 'C' ||positionType === '1B'|| positionType === '2B' || positionType === '3B'|| positionType === 'SS'||
+	positionType === 'OF' || positionType === 'CF'|| positionType === 'DH'){
+
 var searchedPlayer = $('#playerName').val();
-$.get(currentURL + "/api/"+ searchedPlayer, function(res){ 
+					  
+$.get(currentURL + "/api-nonPitch/"+ searchedPlayer +"/" + positionType, function(res){ 
 for(i=0;i<res.length;i++){
        var b = $('<button>');
        b.attr("value", JSON.stringify(res[i]));
        b.attr("id", "player");
+       if(res[i].nameFirst||res[i].nameLast||res[i].POS||res[i].yearID){
        b.text(res[i].nameFirst + " " + res[i].nameLast + " " + res[i].POS + " " +  res[i].yearID);
+   		}else{
+   		$("#results").empty();
+   		$("#results").append("<h1>"+ "PlayBall!" + "</h1>")
+   		}
        b.addClass("class","btn");
        b.addClass("btn-primary");
        $("#results").append(b);
       }
 });
-})
+}else{
+	var searchedPlayer = $('#playerName').val();				
+	//Work on this get request tomorrow morning with different tables and clean this up
+	$.get(currentURL + "/api-Pitch/"+ searchedPlayer +"/" + positionType, function(res){ 
+for(i=0;i<res.length;i++){
+       var b = $('<button>');
+       b.attr("value", JSON.stringify(res[i]));
+       b.attr("id", "player");
+       if(res[i].nameFirst||res[i].nameLast||res[i].POS||res[i].yearID){
+       b.text(res[i].nameFirst + " " + res[i].nameLast + " " + res[i].POS + " " +  res[i].yearID);
+   		}else{
+   		$("#results").empty();
+   		$("#results").append("<h1>"+ "PlayBall!" + "</h1>")
+   		}
+       b.addClass("class","btn");
+       b.addClass("btn-primary");
+       $("#results").append(b);
+      }
+});
+}
+
+
+
+}); 
 
 
 $("body").on("click","#player" ,function(e){
@@ -85,6 +118,7 @@ if(team[i] != undefined && team.length === 9){
  }
 });
 
+// Think of how to make it show in results array
 
  // for(i=0; i<team.length; i++){
 	// $("#team").append(team[i].nameFirst + " "+team[i].nameLast + "</br>");
